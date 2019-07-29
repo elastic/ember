0d065c2c78877f8b32aa5f9929a960290bd1e917
Phil Roth <proth@endgame.com>
2019-07-29 14:49:22 -0400

    changing 'year' to 'feature_version'

--- scripts/train_ember.py
+++ scripts/train_ember.py
@@ -10,7 +10,7 @@ def main():
     prog = "train_ember"
     descr = "Train an ember model from a directory with raw feature files"
     parser = argparse.ArgumentParser(prog=prog, description=descr)
-    parser.add_argument("-y", "--year", type=int, default=2018, help="EMBER data year")
+    parser.add_argument("-v", "--featureversion", type=int, default=2, help="EMBER feature version")
     parser.add_argument("datadir", metavar="DATADIR", type=str, help="Directory with raw features")
     parser.add_argument("--optimize", help="gridsearch to find best parameters", action="store_true")
     args = parser.parse_args()
@@ -22,7 +22,7 @@ def main():
     y_train_path = os.path.join(args.datadir, "y_train.dat")
     if not (os.path.exists(X_train_path) and os.path.exists(y_train_path)):
         print("Creating vectorized features")
-        ember.create_vectorized_features(args.datadir, args.year)
+        ember.create_vectorized_features(args.datadir, args.featureversion)
 
     params = {
         "boosting": "gbdt",
@@ -40,7 +40,7 @@ def main():
         print(json.dumps(params, indent=2))
 
     print("Training LightGBM model")
-    lgbm_model = ember.train_model(args.datadir, params, args.year)
+    lgbm_model = ember.train_model(args.datadir, params, args.featureversion)
     lgbm_model.save_model(os.path.join(args.datadir, "model.txt"))
 
 
